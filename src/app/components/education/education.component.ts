07a20192e3091e80ccfa35db135e811c16e28b0f
alvaro rodrigues <alvim.lopez@gmail.com>
2024-02-20 14:07:56 -0300

    Education

--- src/app/components/education/education.component.ts
+++ src/app/components/education/education.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Education } from '../../models/models';
 
 @Component({
   selector: 'app-education',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrl: './education.component.css'
 })
 export class EducationComponent {
+  educationList: Education[] = [
+    {
+      institute: 'Unip University',
+      course: 'Computer sciences',
+      duration: '2016-2022',
+      score: '100%'
+    },
+    {
+      institute: 'Ipog, postgraduate',
+      course: 'data sciences',
+      duration: '2023-2025',
+      score: '68%'
+    },
+    {
+      institute: 'Cambly English',
+      course: 'English - Level C1',
+      duration: '2019-2024',
+      score: '100%'
+    },
+  ];
 
 }
