0818ead0447284268b795692b880f063f58a0e17
alvaro rodrigues <alvim.lopez@gmail.com>
2024-02-20 18:25:17 -0300

    Skills

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Skill } from '../../models/models';
 
 @Component({
   selector: 'app-skills',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrl: './skills.component.css'
 })
 export class SkillsComponent {
+  skills: Skill[] = [
+    {
+      name: 'Angular, Angular Meterial',
+      level: 'Expert',
+      rating: 85,
+    },
+    {
+      name: 'Java SE/EE - version 8,11,17',
+      level: 'Expert',
+      rating: 80,
+    },
+    {
+      name: 'HTML, CSS, JavaScrip',
+      level: 'Intermediate',
+      rating: 70,
+    },
+    {
+      name: 'OracleSQL, Postgred, MySQL',
+      level: 'Intermediate',
+      rating: 60,
+    },
+    {
+      name: 'Git, Azure',
+      level: 'Intermediate',
+      rating: 80,
+    },
+  ]
 
 }
--- src/app/models/models.ts
+++ src/app/models/models.ts
@@ -11,3 +11,9 @@ export interface WorkExperience {
   duration: string;
   description: string[];
 }
+
+export interface Skill {
+  name: string;
+  level: string;
+  rating: number;
+}
