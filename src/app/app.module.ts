a818648e41c4b332d9824e6a7454036db4606541
alvaro rodrigues <alvim.lopez@gmail.com>
2024-02-19 21:08:25 -0300

    Creating Responsive Directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { PersonalInformationComponent } from './components/personal-information/personal-information.component';
+import { EducationComponent } from './components/education/education.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ResponsiveDirective } from './directives/responsive.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    PersonalInformationComponent,
+    EducationComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    ContactComponent,
+    ResponsiveDirective
   ],
   imports: [
     BrowserModule,
